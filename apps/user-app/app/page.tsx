10698ec3ed33ebaa7eb8c2b862946dcb7fffe6c6
Aman kalal <amankalal206@gmail.com>
2025-01-03 01:21:20 +0530

    added next-auth

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import { PrismaClient } from "@repo/db/prisma";
+
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
     <div className={styles.page}>
       <h1>Welcome to User App</h1>
       <Image src="/logo.png" alt="Repo Logo" width={200} height={200} />
-      <Button label="Click me" />
     </div>
   );
 }
